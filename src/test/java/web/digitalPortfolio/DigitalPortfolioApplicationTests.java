6393a5c82b6bed78804b1c2b698a81fcfe01d69c
AvsP <avsp1314@gmail.com>
2024-03-13 15:55:02 +0500

    first commit

---
+++ src/test/java/web/digitalPortfolio/DigitalPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package web.digitalPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
